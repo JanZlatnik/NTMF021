c3a37dfadcec7114dd19e6669ab779d4daa0fa7f
Zlatnik Jan Bc. <zlatnikj@hpc-head.troja.mff.cuni.cz>
2025-12-05 21:52:44 +0100

    Restructured project into RandomWalk and Percolation folders

---
+++ RandomWalk/job.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# --- SBATCH File Settings ---
+#SBATCH --job-name=RandomWalk
+#SBATCH --output=output.log
+#SBATCH --error=error.log
+
+# --- SBATCH Partition Settings ---
+#SBATCH --partition=ffa
+#SBATCH --time=11:59:00
+
+# --- SBATCH Computation settings Settings ---
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=64
+#SBATCH --mem-per-cpu=1G
+
+
+echo "=========================================================="
+echo "Job ID: $SLURM_JOB_ID"
+echo "Running on node: $SLURM_NODELIST"
+echo "Number of CPUs: $SLURM_CPUS_PER_TASK"
+echo "Working directory: $(pwd)" 
+echo "=========================================================="
+
+# Modules loading
+echo "Loading modules..."
+module load oneapi/tbb oneapi/compiler-rt oneapi/umf oneapi/mkl/latest oneapi/compiler-intel-llvm/latest
+
+# OMP Settings
+echo "OMP Number of threads: $SLURM_CPUS_PER_TASK"
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OMP_MAX_ACTIVE_LEVELS=1
+
+# Running computation
+echo "Running computation $HOME/NTMF021/random_walk"
+$HOME/NTMF021/random_walk
+
+echo "=========================================================="
+echo "Computation finished."
+echo "Job ending."
+echo "=========================================================="
